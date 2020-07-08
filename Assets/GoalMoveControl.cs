684ef0ffc1466c3a475858bd5449c17c6246ce36
saekitomonori <saeki.tomonori.17@shizuoka.ac.jp>
2020-07-08 15:40:13 +0900

    add handle controller

---
+++ Assets/GoalMoveControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalMoveControl : MonoBehaviour
+{
+    private Vector3 targetpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time) * 1.0f + targetpos.x, targetpos.y, targetpos.z);
+    }
+}
---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/TransformObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class TransformObject : MonoBehaviour
+    {
+        private CarController m_CarController;
+
+        private void Awake()//起動時に一回だけ動く
+        {
+            // get the car controller reference
+            m_CarController = GetComponent<CarController>();
+
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            this.transform.position += new Vector3(0, 0, 0.01f);
+        }
+    }
+}
