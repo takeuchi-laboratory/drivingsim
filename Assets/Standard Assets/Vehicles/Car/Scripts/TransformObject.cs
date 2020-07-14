25e9a8b8144af678252a6238a030e55543ecdee7
saekitomonori <saeki.tomonori.17@shizuoka.ac.jp>
2020-07-14 18:13:49 +0900

    add testscript

--- Assets/Standard Assets/Vehicles/Car/Scripts/TransformObject.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/TransformObject.cs
@@ -25,12 +25,21 @@ public class TransformObject : MonoBehaviour
         void FixedUpdate()
         {
         //var forward = _transform.forward;
+        //rigidbodyvpp.velocity.magnitude vppの速度
         //rigidbody.velocity = new Vector3(0, 0, 1f); //Carの正面方向(z方向)に速度1m/sを与える
         Vector3 carpos = transform.position;
         Vector3 vpppos = vpp.transform.position;
         float dis = Vector3.Distance(carpos, vpppos);
         Debug.Log("車間距離:" + dis);
-         rigidbody.velocity = new Vector3(0, 0, rigidbodyvpp.velocity.magnitude);
+         //rigidbody.velocity = new Vector3(0, 0, rigidbodyvpp.velocity.magnitude);
+        if(dis <= 20)
+        {
+            rigidbody.velocity = new Vector3(0, 0, rigidbodyvpp.velocity.magnitude*1.2f);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector3(0, 0, 5f);
+        }
             //myTransform = this.transform;
             Debug.Log("carの速度" + rigidbody.velocity.magnitude + "m/s");    //速さの出力
             //Debug.Log("carのz座標:" + transform.position.z);
